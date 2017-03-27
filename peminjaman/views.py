aa2d9f61b40ec34715e26b3f8fbd4442f3b0837c
harryawk <harry.kambey@gmail.com>
2017-03-27 16:14:17 +0700

    add edit backend logic

--- peminjaman/views.py
+++ peminjaman/views.py
@@ -40,6 +40,7 @@ def formadd(request):
 
         obj_peminjam = Peminjam.objects.get(id=input_peminjam)
         obj_ruangan = Ruangan.objects.get(id=input_ruangan)
+        errormsg = ''
         try:
             Peminjaman.objects.get(peminjam=obj_peminjam, ruangan=obj_ruangan, waktu_awal=tanggal_mulai_pinjam, waktu_akhir=tanggal_selesai_pinjam)
         except Peminjaman.DoesNotExist:
@@ -49,6 +50,8 @@ def formadd(request):
                                          waktu_akhir=tanggal_selesai_pinjam,
                                          deskripsi=input_deskripsi)
             new_transaction.save()
+            return HttpResponseRedirect("/peminjaman/")
+
     # Peminjam.objects.get(peminjam=input_peminjam)
     # Ruangan.objects.get(ruangan=input_ruangan)
     all_peminjam = Peminjam.objects.all()
@@ -61,7 +64,56 @@ def formadd(request):
 
 # Return a form which'll be used to edit peminjaman object to model
 def formedit(request, peminjaman_id):
-    return render(request, 'peminjaman/edit.html', {})
+
+    print "===========FORMEDIT==========="
+
+    object_peminjaman = Peminjaman.objects.get(id=peminjaman_id)
+    if request.method == 'POST':
+        # Ambil data hasil input dari user
+        input_peminjam = request.POST['peminjam']
+        input_ruangan = request.POST['ruangan']
+
+        tanggal_mulai_pinjam = request.POST['waktu_awal_0']  # format tanggal : %Y-%m-%d
+        tanggal_mulai_pinjam = datetime.strptime(tanggal_mulai_pinjam, "%Y-%m-%d")
+        print "tanggal_mulai_pinjam : ", tanggal_mulai_pinjam
+        pukul_mulai_pinjam = request.POST['waktu_awal_1']  # format waktu : %H:%M
+        x = datetime.strptime(pukul_mulai_pinjam, "%H:%M")
+        # print datetime.time()
+        tanggal_mulai_pinjam = tanggal_mulai_pinjam.replace(hour=x.hour, minute=x.minute)
+        print "Setelah : " + str(tanggal_mulai_pinjam)
+        print x.hour, x.minute, x.second
+
+        tanggal_selesai_pinjam = request.POST['waktu_akhir_0']
+        tanggal_selesai_pinjam = datetime.strptime(tanggal_selesai_pinjam, "%Y-%m-%d")
+        pukul_selesai_pinjam = request.POST['waktu_akhir_1']
+        y = datetime.strptime(pukul_selesai_pinjam, "%H:%M")
+        tanggal_selesai_pinjam = tanggal_selesai_pinjam.replace(hour=y.hour, minute=y.minute)
+        input_deskripsi = request.POST['deskripsi']
+
+        obj_peminjam = Peminjam.objects.get(id=input_peminjam)
+        obj_ruangan = Ruangan.objects.get(id=input_ruangan)
+        errormsg = ''
+        try:
+            Peminjaman.objects.get(peminjam=obj_peminjam, ruangan=obj_ruangan, waktu_awal=tanggal_mulai_pinjam,
+                                   waktu_akhir=tanggal_selesai_pinjam)
+        except Peminjaman.DoesNotExist:
+            object_peminjaman.peminjam=obj_peminjam
+            object_peminjaman.ruangan=obj_ruangan
+            object_peminjaman.waktu_awal=tanggal_mulai_pinjam
+            object_peminjaman.waktu_akhir=tanggal_selesai_pinjam
+            object_peminjaman.deskripsi=input_deskripsi
+            print "///////////SAVE/////////////"
+            object_peminjaman.save()
+            return HttpResponseRedirect("/peminjaman/")
+
+    all_peminjam = Peminjam.objects.all()
+    all_ruangan = Ruangan.objects.all()
+    return render(request, 'peminjaman/edit.html', {
+        'all_peminjam': all_peminjam,
+        'all_ruangan': all_ruangan,
+        'object_peminjaman': object_peminjaman,
+        'id_peminjaman': peminjaman_id,
+    })
 
 
 # Return a form which'll be used to delete peminjaman object to model
@@ -75,7 +127,7 @@ def formdelete(request, peminjaman_id, errormsg=''):
     pass
     return render(request, 'peminjaman/index.html', {
         'all_peminjaman': all_peminjaman,
-        'error': errormsg
+        'error': errormsg,
     })
     # return render(request, 'peminjaman/delete.html', {})
 

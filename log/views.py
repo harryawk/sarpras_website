aed2c21067422379095b6f70e692a81904c4abc8
Unknown <luthkur@gmail.com>
2017-04-25 12:59:15 +0700

    Fixed bugs of semua option in log filtering

--- log/views.py
+++ log/views.py
@@ -16,10 +16,12 @@ def index(request):
 @login_required
 @csrf_exempt
 def filter(request, select_year,select_month):
-    if select_year == "" and select_month == "":
-        selected_log = Log.objects.all()
+    if select_year == "1971" and select_month == "0":
+        selected_log = Log.objects.values()
+    elif select_month == "0":
+        selected_log = Log.objects.filter(tanggal__year=select_year).values()
     else:
-        selected_log = Log.objects.filter(tanggal__year = select_year, tanggal__month = select_month).values()
+        selected_log = Log.objects.filter(tanggal__year=select_year, tanggal__month=select_month).values()
     return JsonResponse({'results': list(selected_log)})
 
 @login_required

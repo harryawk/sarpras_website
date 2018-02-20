6035b4078a64f9c40803d481da7fb08f6b65f49b
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2018-02-20 12:58:35 +0700

    add static_root settings

--- PeminjamanRuanganSarpras/settings.py
+++ PeminjamanRuanganSarpras/settings.py
@@ -137,7 +137,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_URL = '/login/'
 
